2c3ca136e68fa2f800b5c8d05d58e68e57847ce6
Aniruddha <aniruddha.pradhan@axeno.co>
2025-04-10 13:43:39 +0530

    fixed sortFilter bug

--- src/searchResult.js
+++ src/searchResult.js
@@ -43,7 +43,7 @@ const searchResult = {
     renderProducts(products) {
         const resultDiv = this.container.querySelector(".mainright__results");
         resultDiv.innerHTML = "";
-        if(products.length===0){
+        if (products.length === 0) {
             resultDiv.innerHTML = "<h3>Products Not Found</h3>";
         }
         products.forEach((product) => {
@@ -79,85 +79,87 @@ const searchResult = {
                 </div>
             `;
             const productRating = product.rating;
-            for(let i=1;i<=Math.floor(productRating);i++){
+            for (let i = 1; i <= Math.floor(productRating); i++) {
                 const starDiv = mainDiv.querySelector(`.product--rating${i}`);
-                if(starDiv){
+                if (starDiv) {
                     starDiv.style.color = "#da7a47";
-                }   
+                }
             }
             resultDiv.appendChild(mainDiv);
         });
     },
-    slidebar: function(products){
+    slidebar: function (products) {
         const minSlidebar = this.container.querySelector(".mainleft__pricerange--slidebarmin");
         const maxSlidebar = this.container.querySelector(".mainleft__pricerange--slidebarmax");
         const minPrice = this.container.querySelector(".minprice");
         const maxPrice = this.container.querySelector(".maxprice");
-        minSlidebar.addEventListener("input", (e)=>{ 
-            if(parseInt(minSlidebar.value)>parseInt(maxSlidebar.value)){
-                minSlidebar.value = maxSlidebar.value-1;
+        minSlidebar.addEventListener("input", (e) => {
+            if (parseInt(minSlidebar.value) > parseInt(maxSlidebar.value)) {
+                minSlidebar.value = maxSlidebar.value - 1;
             }
             minPrice.innerHTML = `${minSlidebar.value}`;
         })
-        maxSlidebar.addEventListener("input", (e)=>{ 
-            if(parseInt(minSlidebar.value)>parseInt(maxSlidebar.value)){
+        maxSlidebar.addEventListener("input", (e) => {
+            if (parseInt(minSlidebar.value) > parseInt(maxSlidebar.value)) {
                 maxSlidebar.value = minSlidebar.value;
             }
             maxPrice.innerHTML = `${maxSlidebar.value}`;
         })
-        
+
         const clickPrice = this.container.querySelector(".mainleft__pricerange--button");
-        clickPrice.addEventListener("click", ()=>{
-            const minprice = parseInt(minSlidebar.value)/100;
-            const maxprice = parseInt(maxSlidebar.value)/100;
-            const filteredProducts = products.filter(product=>{
+        clickPrice.addEventListener("click", () => {
+            const minprice = parseInt(minSlidebar.value) / 100;
+            const maxprice = parseInt(maxSlidebar.value) / 100;
+            const filteredProducts = products.filter(product => {
                 return product.price >= minprice && product.price <= maxprice;
             });
             this.renderProducts(filteredProducts);
         })
     },
-    filterProducts : function(products){
-    const checkboxes = this.container.querySelectorAll(".mainleft input[type='checkbox']");
-    checkboxes.forEach((checkbox) => {
-        checkbox.addEventListener("change", () => {
-            const selectedCategories = Array.from(checkboxes)
-                .filter(cb => cb.checked)
-                .map(cb => cb.value);
+    filterProducts: function (products) {
+        const checkboxes = this.container.querySelectorAll(".mainleft input[type='checkbox']");
+        checkboxes.forEach((checkbox) => {
+            checkbox.addEventListener("change", () => {
+                const selectedCategories = Array.from(checkboxes)
+                    .filter(cb => cb.checked)
+                    .map(cb => cb.value);
                 if (selectedCategories.length === 0) {
                     this.renderProducts(products);
-                }else {
-                const filteredProducts = products.filter(product =>
-                    selectedCategories.includes(product.category)
-                );
-                this.renderProducts(filteredProducts);
-                this.sortProducts(filteredProducts);
-            }
+                    this.sortProducts(products);
+                }
+                else {
+                    const filteredProducts = products.filter(product =>
+                        selectedCategories.includes(product.category)
+                    );
+                    this.renderProducts(filteredProducts);
+                    this.sortProducts(filteredProducts);
+                }
+            });
         });
-    });
     },
-    sortProducts : function(products){
+    sortProducts: function (products) {
         const sortBy = this.container.querySelector(".searchresult__sortby");
-        sortBy.addEventListener("change",(e)=>{
+        sortBy.addEventListener("change", (e) => {
             const sortProducts = [...products];
-            if(e.target.value === "lowtohigh"){
-                sortProducts.sort((a,b)=>{
-                    return a.price-b.price;
+            if (e.target.value === "lowtohigh") {
+                sortProducts.sort((a, b) => {
+                    return a.price - b.price;
                 });
             }
-            else if(e.target.value === "hightolow"){
-                sortProducts.sort((a,b)=>{
-                    return b.price-a.price;
+            else if (e.target.value === "hightolow") {
+                sortProducts.sort((a, b) => {
+                    return b.price - a.price;
                 })
             }
-            else if(e.target.value === "customerreview"){
-                sortProducts.sort((a,b)=>{
-                    return b.rating-a.rating;
+            else if (e.target.value === "customerreview") {
+                sortProducts.sort((a, b) => {
+                    return b.rating - a.rating;
                 })
             }
-            else if(e.target.value === "featured"){}
+            else if (e.target.value === "featured") { }
             this.renderProducts(sortProducts);
         })
-        
+
     }
 };
 
