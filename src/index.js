ff6809ca3fa14f754e4837dc8878e7fbcbd76acb
Aniruddha <aniruddha.pradhan@axeno.co>
2025-04-10 11:56:43 +0530

    completed assignment of amazon

---
+++ src/index.js
@@ -0,0 +1,217 @@
+const amazonUser = {
+    container: "",
+    data: {
+        location: {
+            city: "Gurugram",
+            pin: "12208",
+        },
+        searchCategories: {
+            all: "All Categories",
+            alexaSkills: "Alexa Skills",
+            amazonClinic: "Amazon Clinic",
+            amazonDevices: "Amazon Devices",
+            amazonFresh: "Amazon Fresh",
+            amazonPharmacy: "Amazon Pharmacy",
+            warehouseDeals: "Amazon Warehouse Deals",
+            appliances: "Appliances",
+            mobileApps: "Apps & Games",
+            artsCrafts: "Arts, Crafts & Sewing",
+            audible: "Audible Books & Originals",
+            automotive: "Automotive Parts & Accessories",
+            babyProducts: "Baby",
+            beauty: "Beauty & Personal Care",
+            stripbooks: "Books",
+            popular: "CDs & Vinyl",
+            mobile: "Cell Phones & Accessories",
+            fashion: "Clothing, Shoes & Jewelry",
+            fashionWomens: "Clothing, Shoes & Jewelry - Women",
+            fashionMens: "Clothing, Shoes & Jewelry - Men",
+            fashionGirls: "Clothing, Shoes & Jewelry - Girls",
+            fashionBoys: "Clothing, Shoes & Jewelry - Boys",
+            fashionBaby: "Clothing, Shoes & Jewelry - Baby",
+            collectibles: "Collectibles & Fine Art",
+            computers: "Computers",
+            financial: "Credit and Payment Cards",
+            digitalMusic: "Digital Music",
+            electronics: "Electronics",
+            lawngarden: "Garden & Outdoor",
+            giftCards: "Gift Cards",
+            grocery: "Grocery & Gourmet Food",
+            handmade: "Handmade",
+            hpc: "Health, Household & Baby Care",
+            localServices: "Home & Business Services",
+            garden: "Home & Kitchen",
+            industrial: "Industrial & Scientific",
+            primeExclusive: "Just for Prime",
+            digitalText: "Kindle Store",
+            fashionLuggage: "Luggage & Travel Gear",
+            luxury: "Luxury Stores",
+            magazines: "Magazine Subscriptions",
+            moviesTv: "Movies & TV",
+            mi: "Musical Instruments",
+            officeProducts: "Office Products",
+            pets: "Pet Supplies",
+            luxuryBeauty: "Premium Beauty",
+            instantVideo: "Prime Video",
+            smartHome: "Smart Home",
+            software: "Software",
+            sporting: "Sports & Outdoors",
+            specialtyApsSns: "Subscribe & Save",
+            subscribeWithAmazon: "Subscription Boxes",
+            tools: "Tools & Home Improvement",
+            toysAndGames: "Toys & Games",
+            underTenDollars: "Under $10",
+            videogames: "Video Games",
+            wholefoods: "Whole Foods Market"
+        },
+    },
+    init: function (container) {
+        this.container = container;
+        this.renderDetails();
+        this.getSearchResult();
+    },
+    renderDetails: function () {
+        this.container.querySelector(".header__address--city").innerHTML = `Delivering to ${this.data.location.city} ${this.data.location.pin}`;
+        const searchCategory = this.container.querySelector(".header__search--category");
+        for (const key in this.data.searchCategories) {
+            const newOption = document.createElement('option');
+            newOption.textContent = this.data.searchCategories[key];
+            newOption.value = key;
+            searchCategory.appendChild(newOption);
+        }
+
+    },
+    getSearchResult: function () {
+        const resultHistory = this.container.querySelector(".resulthistory");
+        
+        const fetchResult = (value) => {
+            if (value === "") {
+                const resultbox = this.container.querySelector(".suggestedresult");
+                resultbox.style.display = "none";
+                resultHistory.style.display = "flex";
+                this.getResultHistory();
+                return;
+            }
+            else {
+                resultHistory.style.display = "none";
+            }
+            fetch(`https://dummyjson.com/products/search?q=${value}`)
+                .then((res) => {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                })
+                .then((res) => {
+                    const products = Array.from(res.products);
+                    this.createSuggestedSearch(products.slice(0, 10));
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        }
+        let debounceTimer;
+        const searchInput = this.container.querySelector(".header__search--inputbox");
+        searchInput.addEventListener("input", (e) => {
+            clearTimeout(debounceTimer);
+            debounceTimer = setTimeout(()=>{
+                fetchResult(e.target.value);
+            },300);   
+        })
+        searchInput.addEventListener("click", (e) => {
+            e.stopPropagation();
+            fetchResult(e.target.value);
+        })
+        const resultbox = this.container.querySelector(".suggestedresult");
+        this.container.addEventListener("click", (e) => {
+            if (!searchInput.contains(e.target) && !resultbox.contains(e.target)) {
+                resultbox.style.display = "none";
+            }
+            else {
+                resultbox.style.display = "flex";
+            }
+        })
+        const submitBtn = this.container.querySelector(".header__search--submit");
+        submitBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            const history = JSON.parse(localStorage.getItem("localStorageSearchHistory")) || [];
+            console.log(searchInput.value);
+            if (searchInput.value.trim()) {
+                redirect(searchInput.value);
+                history.unshift(searchInput.value);
+                localStorage.setItem("localStorageSearchHistory", JSON.stringify(history));
+            }
+        })
+    },
+    getResultHistory: function () {
+        const local = localStorage.getItem('localStorageSearchHistory');
+        let searchHistoryResults = JSON.parse(local);
+        searchHistoryResults = searchHistoryResults.slice(0, 10);
+        const resultHistory = this.container.querySelector(".resulthistory");
+        resultHistory.innerHTML = "";
+        if (local) {
+            for (let key in searchHistoryResults) {
+                const newResult = document.createElement("a");
+                const crossicon = document.createElement("img");
+                crossicon.src = "./images/cross_icon.svg";
+                crossicon.alt = "cross icon";
+                newResult.innerHTML = `${searchHistoryResults[key]}`;
+                newResult.appendChild(crossicon);
+                resultHistory.appendChild(newResult);
+                crossicon.addEventListener("click", (e) => {
+                    this.deleteItemFromStorage(searchHistoryResults[key]);
+                    e.stopPropagation();
+                });
+                newResult.addEventListener("click", () => {
+                    redirect(searchHistoryResults[key]);
+                })
+            }
+        }
+        const searchInput = this.container.querySelector(".header__search--inputbox");
+        this.container.addEventListener("click", (e) => {
+            if (!searchInput.contains(e.target) && !resultHistory.contains(e.target)) {
+                resultHistory.style.display = "none";
+            }
+            else {
+                resultHistory.style.display = "flex";
+            }
+        })
+    },
+    createSuggestedSearch: function (result) {
+        const resultbox = this.container.querySelector(".suggestedresult");
+        resultbox.style.display = "flex";
+        resultbox.innerHTML = "";
+        for (let i = 0; i < result.length; i++) {
+            const newResult = document.createElement("a");
+            const searchicon = document.createElement("img");
+            searchicon.src = "./images/search_icon.svg";
+            searchicon.alt = "search icon";
+            newResult.appendChild(searchicon);
+            newResult.innerHTML += `${result[i].title}`;
+            resultbox.appendChild(newResult);
+            newResult.addEventListener("click", () => {
+                redirect(result[i].title);
+            })
+        }
+    },
+    deleteItemFromStorage: function (historyKey) {
+        let history = JSON.parse(localStorage.getItem("localStorageSearchHistory")) || [];
+        console.log(history);
+        console.log(historyKey);
+        history = Array.from(history);
+        console.log(history);
+        history = history.filter(item => item !== historyKey);
+        localStorage.setItem("localStorageSearchHistory", JSON.stringify(history));
+        this.getResultHistory();
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const containers = document.querySelectorAll(".wrapper");
+    containers.forEach((container) => {
+        amazonUser.init(container);
+    })
+})
+function redirect(value) {
+    window.location.href = `./searchResult.html?search=${value}`;
+}
---
+++ src/searchResult.js
@@ -0,0 +1,169 @@
+const searchResult = {
+    container: "",
+    value: "",
+    init: function (container) {
+        this.container = container;
+        this.fetchQuery();
+        this.fetchResult();
+    },
+    fetchQuery: function () {
+        const params = new URLSearchParams(window.location.search);
+        const value = params.get("search");
+        const searchInput = this.container.querySelector(
+            ".header__search--inputbox"
+        );
+        searchInput.value = value;
+        this.value = value;
+    },
+    fetchResult: function () {
+        fetch(`https://dummyjson.com/products/search?q=${this.value}`)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+            })
+            .then((res) => {
+                const products = Array.from(res.products);
+                this.getResultHeader(products.length);
+                this.sortProducts(products);
+                this.filterProducts(products);
+                this.slidebar(products);
+                this.renderProducts(products);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    getResultHeader: function (length) {
+        const resultfoundDiv = this.container.querySelector(
+            ".searchresult__desc--left"
+        );
+        resultfoundDiv.innerHTML = `${length} results found for <span>"${this.value}" </span>`;
+    },
+    renderProducts(products) {
+        const resultDiv = this.container.querySelector(".mainright__results");
+        resultDiv.innerHTML = "";
+        if(products.length===0){
+            resultDiv.innerHTML = "<h3>Products Not Found</h3>";
+        }
+        products.forEach((product) => {
+            const mainDiv = document.createElement("div");
+            mainDiv.classList.add("mainright__results--product");
+            let price = product.price * 100;
+            const discount = (price * product.discountPercentage) / 100;
+            const finalPrice = (Math.floor(price - discount)).toLocaleString();
+            price = (Math.floor(price)).toLocaleString();
+            mainDiv.innerHTML = `
+                  <div class="product__image">
+                    <img class="product__image--img" src=${product.thumbnail}>
+                  </div>
+                  <div class="product__details">
+                    <h2 class="product__details--title">${product.title}: ${product.description}</h2>
+                    <div class="product__details--ratingdiv">
+                      <p class="product__details--rating product--rating1">★</p>
+                      <p class="product__details--rating product--rating2">★</p>
+                      <p class="product__details--rating product--rating3">★</p>
+                      <p class="product__details--rating product--rating4">★</p>
+                      <p class="product__details--rating product--rating5" >★</p>
+                      <span class="product__details--ratingdiv--reviewcount">${product.reviews.length}</span>
+                    </div>
+                    <div class="product__details--price">
+                      <span>₹</span><h2>${finalPrice}</h2>
+                      <div>M.R.P: <s>${price}</s></div><p>(${product.discountPercentage}% off)</p>
+                    </div>
+                    <p class="product__details--save">Save Extra with No Cost EMI</p>
+                    <p>${product.returnPolicy}</p>
+                    <button class="product__details--addcart">Add to cart</button>
+
+                  </div>
+                </div>
+            `;
+            const productRating = product.rating;
+            for(let i=1;i<=Math.floor(productRating);i++){
+                const starDiv = mainDiv.querySelector(`.product--rating${i}`);
+                if(starDiv){
+                    starDiv.style.color = "#da7a47";
+                }   
+            }
+            resultDiv.appendChild(mainDiv);
+        });
+    },
+    slidebar: function(products){
+        const minSlidebar = this.container.querySelector(".mainleft__pricerange--slidebarmin");
+        const maxSlidebar = this.container.querySelector(".mainleft__pricerange--slidebarmax");
+        const minPrice = this.container.querySelector(".minprice");
+        const maxPrice = this.container.querySelector(".maxprice");
+        minSlidebar.addEventListener("input", (e)=>{ 
+            if(parseInt(minSlidebar.value)>parseInt(maxSlidebar.value)){
+                minSlidebar.value = maxSlidebar.value-1;
+            }
+            minPrice.innerHTML = `${minSlidebar.value}`;
+        })
+        maxSlidebar.addEventListener("input", (e)=>{ 
+            if(parseInt(minSlidebar.value)>parseInt(maxSlidebar.value)){
+                maxSlidebar.value = minSlidebar.value;
+            }
+            maxPrice.innerHTML = `${maxSlidebar.value}`;
+        })
+        
+        const clickPrice = this.container.querySelector(".mainleft__pricerange--button");
+        clickPrice.addEventListener("click", ()=>{
+            const minprice = parseInt(minSlidebar.value)/100;
+            const maxprice = parseInt(maxSlidebar.value)/100;
+            const filteredProducts = products.filter(product=>{
+                return product.price >= minprice && product.price <= maxprice;
+            });
+            this.renderProducts(filteredProducts);
+        })
+    },
+    filterProducts : function(products){
+    const checkboxes = this.container.querySelectorAll(".mainleft input[type='checkbox']");
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener("change", () => {
+            const selectedCategories = Array.from(checkboxes)
+                .filter(cb => cb.checked)
+                .map(cb => cb.value);
+                if (selectedCategories.length === 0) {
+                    this.renderProducts(products);
+                }else {
+                const filteredProducts = products.filter(product =>
+                    selectedCategories.includes(product.category)
+                );
+                this.renderProducts(filteredProducts);
+                this.sortProducts(filteredProducts);
+            }
+        });
+    });
+    },
+    sortProducts : function(products){
+        const sortBy = this.container.querySelector(".searchresult__sortby");
+        sortBy.addEventListener("change",(e)=>{
+            const sortProducts = [...products];
+            if(e.target.value === "lowtohigh"){
+                sortProducts.sort((a,b)=>{
+                    return a.price-b.price;
+                });
+            }
+            else if(e.target.value === "hightolow"){
+                sortProducts.sort((a,b)=>{
+                    return b.price-a.price;
+                })
+            }
+            else if(e.target.value === "customerreview"){
+                sortProducts.sort((a,b)=>{
+                    return b.rating-a.rating;
+                })
+            }
+            else if(e.target.value === "featured"){}
+            this.renderProducts(sortProducts);
+        })
+        
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const containers = document.querySelectorAll(".wrapper");
+    containers.forEach((container) => {
+        searchResult.init(container);
+    });
+});
